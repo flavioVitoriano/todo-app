a433ff06f30b2915727b68d6bee6361b2610fb50
fvitoriano <flavio.vitorianodev@gmail.com>
2020-09-10 17:38:30 -0300

    added cors middleware

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 import crud
@@ -9,6 +10,16 @@ from database import SessionLocal
 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Dependency
