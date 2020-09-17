da34dc1d2fe6d570f574ff9bbe43219f4f14f0df
fvitoriano <flavio.vitorianodev@gmail.com>
2020-09-17 12:12:31 -0300

    added toast

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './styles/global.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Routes from './routes';
 import { StoreContext, RootInstance } from 'Store';
+import { ToastContainer } from 'react-toastify';
+
 
 function App() {
   return (
     <StoreContext.Provider value={RootInstance}>
+      <ToastContainer />
       <Routes />
     </StoreContext.Provider>
   );
